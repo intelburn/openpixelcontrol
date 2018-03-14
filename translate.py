c8b4a3da3dabff11b3b7ef07ac64485a421a2c8a
intelburn <nerdacs@gmail.com>
2018-03-14 13:35:23 -0400

    added ability to send to simulator

--- translate.py
+++ translate.py
@@ -2,6 +2,8 @@ import socket
 outside = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 inside = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 outside.bind(("0.0.0.0", 7890))
-while True
+inside.connect(("127.0.0.1",7890))
+while True:
     data, addr = outside.recvfrom(10000)
-    print("data: {0}".format(data))
+    print("data received from {0}".format(addr))
+    inside.send(data)
