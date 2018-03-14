eeaf1a3d066f74bd8611e2ca21057c334e50a39e
intelburn <nerdacs@gmail.com>
2018-03-14 13:55:39 -0400

    increased buffer

--- translate.py
+++ translate.py
@@ -4,6 +4,6 @@ inside = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 outside.bind(("0.0.0.0", 7890))
 inside.connect(("127.0.0.1",7890))
 while True:
-    data, addr = outside.recvfrom(10000)
+    data, addr = outside.recvfrom(50000)
     print("data received from {0}".format(addr))
     inside.send(data)
