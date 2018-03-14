d586fabfc0dd4945f7d098996944da1d9c0fff87
intelburn <nerdacs@gmail.com>
2018-03-14 14:01:28 -0400

    changed print statment, and increased buffer size

--- translate.py
+++ translate.py
@@ -4,6 +4,6 @@ inside = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 outside.bind(("0.0.0.0", 7890))
 inside.connect(("127.0.0.1",7890))
 while True:
-    data, addr = outside.recvfrom(50000)
-    print("data received from {0}".format(addr))
+    data, addr = outside.recvfrom(65000)
+    print("received {1} bytes of data from {0}".format(addr, data.len)
     inside.send(data)
